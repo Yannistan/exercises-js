adf294bcb67ded8fc56f3fdbcc7f01a5883e87c6
Yannistan <tanis.ioannis@gmail.com>
2020-09-22 17:05:59 +0200

    ex.14 resolou

--- src/ex14.js
+++ src/ex14.js
@@ -1,36 +1,28 @@
 function distributeur(somme, code) {
-    const findprix = (code) => {
-        let prix = 0;
-        if (code == 1) {
-            prix = 0.50;
-        } else if (code == 2) {
-            prix == 1.00;
-        } else if (code == 3) {
-            prix == 2.00;
-        } else {
-            prix == 3.00;
-        }
-        return prix;
-    }
-    const accept = (somme, code) => {
-        findprix(code);
 
-        if (somme >= findprix(code)) {
-            return true;
-        } else
-            return false;
+    let prix;
+    if (code == 1) {
+        prix = 0.50;
+    } else if (code == 2) {
+        prix = 1.00;
+    } else if (code == 3) {
+        prix = 2.00;
+    } else if (code == 4) {
+        prix = 3.00;
     }
 
 
 
-    if (accept(somme, code) == true) {
+    if (somme >= prix) {
         let arendre;
-        arendre = somme - findprix(code);
+        arendre = somme - prix;
         return arendre;
-    } else {
-        console.log("Achat non effectué")
-    }
 
+    } else {
+        console.log("Achat non effectué");
+    }
 }
+
+
 
 console.log(distributeur(30, 4));
